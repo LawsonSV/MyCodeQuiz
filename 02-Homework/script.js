d4bd856750a689007a015d1f5c5e22570999d6a3
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-03-24 18:09:15 -0700

    Created timer, removed questions array as I am no longer using it, created more event listeners

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -1,65 +1,41 @@
 let startButton = document.getElementById("start-button");
 let startPage = document.querySelector("#start-page");
-let questionOne = document.getElementById("quest-one");
-let qOneWrongs = document.getElementsByClassName("q-one-wrong");
-let qOneRight = document.getElementById("q-one-right");
 let timeText = document.getElementById("time-left");
 let rightOrWrong = document.getElementById("right-or-wrong");
 let timeLeft = 150;
-let questions = [
-    {
-        question: "Which of the following is an HTML attribute?",
-        answersArr: ["const", "let", "img", "src"],
-        correct: "src"
-    },
-    {
-        question: "To define a letiable in JS we may use the ___ keyword",
-        answersArr: ["header", "let", "style", "function"],
-        correct: "let"
-    },
-    {
-        question: "In CSS we can define colors using their ___ value",
-        answersArr: ["hexadecimal", "binary", "variable", "coloric"],
-        correct: "hexadecimal"
-    },
-    {
-        question: "JS files are linked to HTML docs via the ___ tag",
-        answersArr: ["<a>", "<script>", "<h1>", "<boolean>"],
-        correct: "<script>"
-    },
-    {
-        question: "One can quickly set up the head of their HTML doc by typing which character?",
-        answersArr: ["@", "%", "!", "&"],
-        correct: "!"
-    },
-    {
-        question: "Which of the following is NOT an example of a JS method?",
-        answersArr: ["push()", "slice()", "scrape()", "substring()"],
-        correct: "scrape()"
-    },
-    {
-        question: "To make an item clickable on page, one must implement an:",
-        answersArr: ["eventListener", "clickFollower", "targetAssigner", "buttonAccepter"],
-        correct: "eventListener"
-    },
-    {
-        question: "After clicking the inspect tool in a browser, in which tab can I find error messages?",
-        answersArr: ["Application", "Elements", "Sources", "Console"],
-        correct: "Console"
-    },
-    {
-        question: "The HTML document holds the ___ of the page",
-        answersArr: ["Styling", "Functions", "Content", "None of the above"],
-        correct: "Content"
-    },
-    {
-        question: "The link to the CSS stylesheet should be placed in which section of the HTML doc?",
-        answersArr: ["head", "body", "footer", "None of the above"],
-        correct: "head"
-    }
-];
 
+let questionOne = document.getElementById("quest-one");
+let questionTwo = document.getElementById("quest-two");
+let questionThree = document.getElementById("quest-three");
+let questionFour = document.getElementById("quest-four");
+let questionFive = document.getElementById("quest-five");
+let questionSix = document.getElementById("quest-six");
+let questionSeven = document.getElementById("quest-seven");
+let questionEight = document.getElementById("quest-eight");
+let questionNine = document.getElementById("quest-nine");
+let questionTen = document.getElementById("quest-ten");
 
+let qOneWrongs = document.getElementsByClassName("q-one-wrong");
+let qTwoWrongs = document.getElementsByClassName("q-two-wrong");
+let qThreeWrongs = document.getElementsByClassName("q-three-wrong");
+let qFourWrongs = document.getElementsByClassName("q-four-wrong");
+let qFiveWrongs = document.getElementsByClassName("q-five-wrong");
+let qSixWrongs = document.getElementsByClassName("q-six-wrong");
+let qSevenWrongs = document.getElementsByClassName("q-seven-wrong");
+let qEightWrongs = document.getElementsByClassName("q-eight-wrong");
+let qNineWrongs = document.getElementsByClassName("q-nine-wrong");
+let qTenWrongs = document.getElementsByClassName("q-ten-wrong");
+
+let qOneRight = document.getElementById("q-one-right");
+let qTwoRight = document.getElementById("q-two-right");
+let qThreeRight = document.getElementById("q-three-right");
+let qFourRight = document.getElementById("q-four-right");
+let qFiveRight = document.getElementById("q-five-right");
+let qSixRight = document.getElementById("q-six-right");
+let qSevenRight = document.getElementById("q-seven-right");
+let qEightRight = document.getElementById("q-eight-right");
+let qNineRight = document.getElementById("q-nine-right");
+let qTenRight = document.getElementById("q-ten-right");
 
 startButton.addEventListener("click", function(event) {
     event.preventDefault();
@@ -78,23 +54,47 @@ startButton.addEventListener("click", function(event) {
 for (i = 0; i < qOneWrongs.length; i++)
 qOneWrongs[i].addEventListener("click", function() {
     let qOneText = document.createElement("h3");
-    questionOne.style.display = "none" 
+    questionOne.style.display = "none";
+    questionTwo.style.display = "block";
     qOneText.textContent = "Wrong! 10 seconds lost...";
     rightOrWrong.append(qOneText);
     setTimeout(function(){
-        rightOrWrong.style.display = "none"
+        qOneText.remove()
    }, 1000);
-
    timeLeft-=10;
 });
 
-
 qOneRight.addEventListener("click", function() {
     let qOneText = document.createElement("h3");
-    questionOne.style.display = "none" 
+    questionOne.style.display = "none";
+    questionTwo.style.display = "block";
     qOneText.textContent = "Correct!";
     rightOrWrong.append(qOneText);
     setTimeout(function(){
-        rightOrWrong.style.display = "none"
+        qOneText.remove()
    }, 1000);
-})
+});
+
+for (i = 0; i < qTwoWrongs.length; i++)
+qTwoWrongs[i].addEventListener("click", function() {
+    let qTwoText = document.createElement("h3");
+    questionTwo.style.display = "none";
+    questionThree.style.display = "block";
+    qTwoText.textContent = "Wrong! 10 seconds lost...";
+    rightOrWrong.append(qTwoText);
+    setTimeout(function(){
+       qTwoText.remove();
+   }, 1000);
+   timeLeft-=10;
+});
+
+qTwoRight.addEventListener("click", function() {
+    let qTwoText = document.createElement("h3");
+    questionTwo.style.display = "none";
+    questionThree.style.display = "block";
+    qTwoText.textContent = "Correct!";
+    rightOrWrong.append(qTwoText);
+    setTimeout(function(){
+        qTwoText.remove()
+   }, 1000);
+});
